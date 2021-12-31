b05db3b107412e655d2e066b835a87c32a3cac11
Charles Lapierre <charles.lapierre@bell.net>
2021-12-30 23:38:16 -0500

    Add hit detection both for walls and self.
    Snake now detects when it hits itself and calls game_over() appropriately. Snake also detects when a wall is hit and reacts according to taurus walls or regular walls. For now, game_over() only cretes a new edible but this will be changed

--- Game.py
+++ Game.py
@@ -11,6 +11,9 @@ class Game():
         self.unpaused = False
         self.edible = self.new_edible()
 
+        # Settings
+        self.taurus = True
+
     def start(self):
         self.unpaused = True
         sleep(1)
@@ -22,29 +25,28 @@ class Game():
         self.unpaused = False
         print("Game paused")
 
+    def game_over(self):
+        self.edible = self.new_edible()
+
     def new_edible(self):
         edible_cell = randint(0, 50*30 - 1)
         edible_x = (edible_cell % 50) * 20
         edible_y = (edible_cell // 50) * 20
-        print(f"Coords of edible: {edible_x}, {edible_y}")
+        # print(f"Coords of edible: {edible_x}, {edible_y}")
         return self.game_board.create_oval(edible_x, edible_y, edible_x + 19, edible_y + 19, fill = "red", width = 0)
-        # edible = self.game_board.create_oval(edible_x, edible_y, edible_x + 19, edible_y + 19, fill = "red", width = 0)
-        # self.game_board.update_idletasks()
-        # print(f"Edible created and printed")
-        # return edible
         
     class Snake():
 
         def __init__(self, game):
             self.game = game
             self.game_board = self.game.game_board
-            self.orientation = 0 # Only useful for the head
+            self.orientation = 0
             self.head = self.game_board.create_arc(0, 0, 19, 19, start = 30, extent = 300, fill = "blue", width = 0)
-            self.nodes = [self.game_board.create_oval(-19, 0, -1, 19, fill = "blue", width = 0)] # Nodes are elements in the body of the Snake
+            self.nodes = [self.game_board.create_oval(-20, 0, -1, 19, fill = "blue", width = 0)] # Nodes are elements in the body of the Snake
 
         def orient(self, event, new_orientation):
 
-            if not (self.orientation - new_orientation)%2:
+            if not (self.orientation - new_orientation)%2 or new_orientation == (self.last_move_orientation + 2)%4:
                 return
 
             self.orientation = new_orientation
@@ -52,27 +54,68 @@ class Game():
             self.game_board.update_idletasks()
         
         def advance(self):
+
+            self.last_move_orientation = self.orientation
             
             # Getting coords of head
             coords_of_head = self.game_board.coords(self.head)
+            print(coords_of_head)
 
-            # Adding new bulb in place of head
+            # Adding new node in place of head
             self.nodes.append(self.game_board.create_oval(coords_of_head, fill = "blue"))
             
             # Moving head
-            # print(coords_of_head)
             mod = self.orientation % 2
             
             coords_of_head[mod]     += (-1)**(self.orientation//2)*20
             coords_of_head[mod + 2] += (-1)**(self.orientation//2)*20
 
+            # Wall hits
+            if coords_of_head[0] == 1000.0:
+                if self.game.taurus == True:
+                    coords_of_head[0] = 0
+                    coords_of_head[2] = 19
+                else:
+                    self.game.game_over()
+            elif coords_of_head[0] == -20.0:
+                if self.game.taurus == True:
+                    coords_of_head[0] = 980
+                    coords_of_head[2] = 999
+                else:
+                    self.game.game_over()
+            elif coords_of_head[1] == 600.0:
+                if self.game.taurus == True:
+                    coords_of_head[1] = 0
+                    coords_of_head[3] = 19
+                else:
+                    self.game.game_over()
+            elif coords_of_head[1] == -20.0:
+                if self.game.taurus == True:
+                    coords_of_head[1] = 580
+                    coords_of_head[3] = 599
+                else:
+                    self.game.game_over()
+
             self.game_board.coords(self.head, coords_of_head)
 
+            # Eating
             if self.game_board.coords(self.head) == self.game_board.coords(self.game.edible):
                 self.game_board.delete(self.game.edible)
                 self.game.edible = self.game.new_edible()
             else:
                 self.game_board.delete(self.nodes.pop(0))
 
-            # This line updates the canvas
+            # Update canvas
+            self.game_board.tag_raise(self.head)
             self.game_board.update_idletasks()
+
+            if self.hit_self():
+                self.game.game_over()
+
+            return
+
+        def hit_self(self):
+            for node in self.nodes:
+                if self.game_board.coords(self.head) == self.game_board.coords(node):
+                    return True
+            return False
